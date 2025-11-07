05d20483d16069d881d3f7ba4bab1a9a77a4954f
Gonzalo Murua <gonzaloemurua96@gmail.com>
2025-11-07 18:46:55 -0300

    Mail module modifications

--- src/mail/mailer.config.ts
+++ src/mail/mailer.config.ts
@@ -1,28 +1,37 @@
 /* eslint-disable prettier/prettier */
-// src/mail/mailer.config.ts
 import { MailerOptions } from '@nestjs-modules/mailer';
 import { join } from 'path';
+import { existsSync } from 'fs';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { ConfigService } from '@nestjs/config';
 
-export const mailerConfig = async (configService: ConfigService): Promise<MailerOptions> => ({
-  transport: {
-    host: configService.get<string>('MAIL_HOST'),
-    port: configService.get<number>('MAIL_PORT'),
-    secure: false, // true si usas SSL
-    auth: {
-      user: configService.get<string>('MAIL_USER'),
-      pass: configService.get<string>('MAIL_PASS'),
+export const mailerConfig = async (configService: ConfigService): Promise<MailerOptions> => {
+  // Si el proyecto está compilado, las plantillas estarán en dist/
+  const distTemplatesDir = join(__dirname, 'templates');
+  const srcTemplatesDir = join(process.cwd(), 'src/mail/templates');
+
+  // Detectar automáticamente si existe dist o src
+  const templatesDir = existsSync(distTemplatesDir) ? distTemplatesDir : srcTemplatesDir;
+
+  return {
+    transport: {
+      host: configService.get<string>('MAIL_HOST'),
+      port: configService.get<number>('MAIL_PORT'),
+      secure: false, // true si usas SSL (por ejemplo, puerto 465)
+      auth: {
+        user: configService.get<string>('MAIL_USER'),
+        pass: configService.get<string>('MAIL_PASS'),
+      },
     },
-  },
-  defaults: {
-    from: `"EcoRed" <${configService.get<string>('MAIL_FROM')}>`,
-  },
-  template: {
-    dir: join(__dirname, 'templates'),
-    adapter: new HandlebarsAdapter(),
-    options: {
-      strict: true,
+    defaults: {
+      from: `"EcoRed" <${configService.get<string>('MAIL_FROM')}>`,
     },
-  },
-});
+    template: {
+      dir: templatesDir,
+      adapter: new HandlebarsAdapter(),
+      options: {
+        strict: true,
+      },
+    },
+  };
+};
